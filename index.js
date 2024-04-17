ffea7ded501eb3e693dab76ad7d82b5fda8a666f
Adam Knapp <aknapp124@gmail.com>
2024-04-17 13:52:40 -0700

    Ability to upload photos sent from front end to cloudinary

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 require('dotenv').config();
+const port = process.env.PORT || 3000;
+const cors = require('cors');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(express.json());
+app.use(cors());
+
+// CLOUDINARY
+
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+	secure: true,
+});
+
+console.log(cloudinary.config());
+
+app.post('/uploadToCloudinary', async (req, res) => {
+	const { photo } = req.body;
+	res.send('Image received');
+	try {
+		const result = await cloudinary.uploader.upload(photo);
+
+		console.log(result);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+const connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	password: process.env.DB_PASSWORD,
+	user: process.env.DB_USER,
+	database: process.env.DB_DATABASE,
+});
 
 app.get('/', (req, res) => {
-	return res.status(200).json({
-		message: 'Hello World',
-	});
+	res.send('Request received');
 });
 
 app.listen(port, () => {
